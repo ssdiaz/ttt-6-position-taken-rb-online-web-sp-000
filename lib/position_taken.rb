82572f04f4359c838551b32a9fb9163844ffb68c
Sam D <samidiaz3@gmail.com>
2021-01-04 22:25:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
+
   if board[index] == " " #if index is NOT blank (true)
     board[index].to eq(false)
+  end
 
-end
 end
