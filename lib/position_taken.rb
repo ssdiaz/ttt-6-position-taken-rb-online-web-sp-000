8df15a341e5e160f12ea6826db7d048bf7522d13
Sam D <samidiaz3@gmail.com>
2021-01-04 22:34:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ if  board[index] != " "
 
   elsif board[index] != ""
 
-    els if board[index] != nil #if index is NOT blank (true)
+  else if board[index] != nil #if index is NOT blank (true)
 
 end
 
