efbf04d20271fdbaefbebb99555546245f164bef
Sam D <samidiaz3@gmail.com>
2021-01-04 23:06:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,8 +5,8 @@ def position_taken? (board, index)
 if board[index] == " " || board[index] == "" || board[index] == nil
   board[index] = false
 
-else
-  board[index] = true
+#else
+  #board[index] = true
 
 end
 end
