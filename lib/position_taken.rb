6dfb7aa2d653be352a0724f55a6bd3a2fece596e
Sam D <samidiaz3@gmail.com>
2021-01-04 22:41:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
 
-if board[index] != " "
+board[index] != " "
 
 #elsif board[index] != "" #|| board[index] != nil
 
 
 
-end
+
 end
